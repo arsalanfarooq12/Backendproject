b37a1569873babddf802b7571d53da7af9b1c654
arsalanfarooq12 <145360951+arsalanfarooq12@users.noreply.github.com>
2025-05-02 20:53:36 +0530

    CREATION -USER AND VIDEO MODELS

--- src/index.js
+++ src/index.js
@@ -6,25 +6,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
-
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("Err :", error);
-      throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port :${process.env.PORT}`);
     });
-    app.listen(process.env.PORT, () => {
-      console.log(`app is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("error:", error);
-    throw err;
-  }
-})();
-*/
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!! ", err);
+  });
